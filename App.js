47522cda8fcf79b5088b5fc3fc8ab47d69796507
Mariana Pedreira <142732786+marianapedreiraa@users.noreply.github.com>
2024-05-30 22:18:07 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,141 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
 
-export default function App() {
+const App = () => {
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieData, setMovieData] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permissão de localização não concedida', 'Por favor, conceda permissão de localização para obter a localização.');
+        return;
+      }
+      let locationData = await Location.getCurrentPositionAsync({});
+      setLocation(locationData);
+    })();
+  }, []);
+
+  const handleSearch = async () => {
+    if (movieTitle.trim() === '') {
+      Alert.alert('Aviso', 'Por favor, insira um título de filme válido.');
+      return;
+    }
+    try {
+      const apiKey = 'b8ad17e8'; // Substitua pelo seu próprio API Key
+      const apiUrl = `https://www.omdbapi.com/?t=${movieTitle}&apikey=${apiKey}`;
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      if (data.Response === 'True') {
+        setMovieData(data);
+      } else {
+        Alert.alert('Erro', 'Filme não encontrado. Verifique o título e tente novamente.');
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Erro', 'Houve um problema na busca do filme. Tente novamente mais tarde.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location && (
+        <View>
+          <Text style={styles.subtitle}>Sua Localização</Text>
+          <Text>Latitude: {location.coords.latitude}</Text>
+          <Text>Longitude: {location.coords.longitude}</Text>
+          <MapView
+            style={{ width: '100%', height: 200 }}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+              title="Sua Localização"
+            />
+          </MapView>
+        </View>
+      )}
+      <Text style={styles.title}>Busca de Filmes</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o nome do filme"
+        value={movieTitle}
+        onChangeText={(text) => setMovieTitle(text)}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSearch}>
+        <Text style={styles.buttonText}>PROCURAR</Text>
+      </TouchableOpacity>
+      {movieData && (
+        <View style={styles.movieContainer}>
+          <Text style={styles.movieTitle}>{movieData.Title}</Text>
+          <Text>Ano: {movieData.Year}</Text>
+          <Text>Gênero: {movieData.Genre}</Text>
+          <Text>Diretor: {movieData.Director}</Text>
+          <Text>Prêmios: {movieData.Awards}</Text>
+        </View>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ADD8E6',
+    padding: 20,
+    marginTop: 75,
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+    padding: 8,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#4682B4',
+    borderRadius: 100, 
+    width: 150, 
+    height: 150,
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  movieContainer: {
+    marginTop: 20,
+  },
+  movieTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
+
+export default App;
